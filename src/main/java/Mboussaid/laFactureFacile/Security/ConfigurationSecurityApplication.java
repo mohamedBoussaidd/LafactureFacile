89392cc9f2417db3825844777e13b20a5c25698e
mohamed boussaid <mohamedboussaidpro@outlook.fr>
2024-11-14 21:52:03 +0100

    update cors

--- src/main/java/Mboussaid/laFactureFacile/Security/ConfigurationSecurityApplication.java
+++ src/main/java/Mboussaid/laFactureFacile/Security/ConfigurationSecurityApplication.java
@@ -64,7 +64,7 @@ public class ConfigurationSecurityApplication {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://lff.simplefacture.fr","http://api.lff.simplefacture.fr"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
