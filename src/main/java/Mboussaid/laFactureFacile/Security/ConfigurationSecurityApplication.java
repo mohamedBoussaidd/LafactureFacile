7c2e18419bc2cc0df085eaf0b201f1b6fce6a212
mohamed boussaid <mohamedboussaidpro@outlook.fr>
2024-12-10 02:02:56 +0100

    update entity item, invoice, and DTO, sericepdv and invoice

--- src/main/java/Mboussaid/laFactureFacile/Security/ConfigurationSecurityApplication.java
+++ src/main/java/Mboussaid/laFactureFacile/Security/ConfigurationSecurityApplication.java
@@ -66,8 +66,7 @@ public class ConfigurationSecurityApplication {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        // configuration.setAllowedOrigins(Arrays.asList( "https://simplefacture.fr"));
+        configuration.setAllowedOrigins(Arrays.asList( "https://simplefacture.fr", "http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
